539b689231860c115fed29b5b82ef062a19762c7
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-10-13 12:32:23 -0400

    Add basic parcel test.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/js-with-babel",
   testEnvironment: "node",
   testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
+  transformIgnorePatterns: ["node_modules/(?!(weak-lru-cache|ordered-binary)/)"],
 };
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,3 +1,41 @@
-test("One Plus One Equals Two", () => {
-  expect(1 + 1).toBe(2);
+import Parcel, { createWorkerFarm } from "@parcel/core";
+import { InitialParcelOptions } from "@parcel/types";
+import { NodeFS, MemoryFS, FileSystem } from "@parcel/fs";
+import path from "path";
+
+describe("parcel-namer-custom-dist-structure", () => {
+  let options: InitialParcelOptions;
+  let outputFS: FileSystem;
+  const distDir: string = path.join(__dirname, "dist");
+  const configDir: string = path.join(__dirname, ".parcelrc-no-reporters");
+  const entries: string = path.join(__dirname, "test");
+  beforeEach(() => {
+    const workerFarm = createWorkerFarm({ maxConcurrentWorkers: 0 });
+    const inputFS = new NodeFS();
+    outputFS = new MemoryFS(workerFarm);
+    options = {
+      entries,
+      shouldDisableCache: true,
+      logLevel: "none",
+      defaultConfig: configDir,
+      inputFS,
+      outputFS,
+      workerFarm,
+      shouldContentHash: true,
+      defaultTargetOptions: {
+        distDir,
+        engines: {
+          browsers: ["last 1 Chrome version"],
+          node: "8",
+        },
+      },
+    };
+  });
+  it("bundles a basic project", async () => {
+    const parcel = new Parcel(options);
+    expect(parcel).toBeDefined();
+    await parcel.run();
+    const outputFileNames = await outputFS.readdir(distDir);
+    expect(outputFileNames).toEqual(["index.html", "a.js", "a.js.map"]);
+  });
 });
