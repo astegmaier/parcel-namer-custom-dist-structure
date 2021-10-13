d63a85f5913f0aefbb8ddca3316809d576d3f359
Andrew Stegmaier <andrew.stegmaier@gmail.com>
2021-10-13 13:04:44 -0400

    Cleanup tests.

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,14 +4,16 @@ import { NodeFS, MemoryFS, FileSystem } from "@parcel/fs";
 import path from "path";
 
 describe("parcel-namer-custom-dist-structure", () => {
-  let options: InitialParcelOptions;
-  let outputFS: FileSystem;
   const distDir: string = path.join(__dirname, "dist");
   const configFile: string = path.join(__dirname, ".parcelrc");
   const entries: string = path.join(__dirname, "test");
+
+  const inputFS = new NodeFS();
+  let outputFS: FileSystem;
+  let options: InitialParcelOptions;
+
   beforeEach(() => {
     const workerFarm = createWorkerFarm({ maxConcurrentWorkers: 0 });
-    const inputFS = new NodeFS();
     outputFS = new MemoryFS(workerFarm);
     options = {
       entries,
@@ -31,12 +33,14 @@ describe("parcel-namer-custom-dist-structure", () => {
       },
     };
   });
+
   it("puts .js bundles in a 'scripts' folder", async () => {
     const parcel = new Parcel(options);
-    expect(parcel).toBeDefined();
     await parcel.run();
+
     const output = await outputFS.readdir(distDir);
     expect(output).toEqual(["scripts", "index.html"]);
+
     const scriptsOutput = await outputFS.readdir(path.join(distDir, "scripts"));
     expect(scriptsOutput).toEqual(["a.js", "a.js.map"]);
   });
